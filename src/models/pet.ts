fc511140435d5e53fcd4fa60bc65a7ad79e27e14
Samuel Vidal <samuelsv365@gmail.com>
2023-02-01 23:15:19 -0300

    final project 1.0

---
+++ src/models/pet.ts
@@ -0,0 +1,157 @@
+type PetType = 'dog' | 'cat' | 'fish';
+type PetSex = 'Macho' | 'Fêmea';
+type Pet = {
+    type: PetType,
+    image: string,
+    name: string,
+    color: string,
+    sex: PetSex
+};
+
+
+const data: Pet[] = [
+    {
+        type: 'dog',
+        image: 'pastor-alemao.jpg',
+        name: 'Pastor-alemão',
+        color: 'Amarelo e Preto',
+        sex: 'Macho'
+    },
+    {
+        type: 'dog',
+        image: 'labrador.jpg',
+        name: 'Labrador-retriever',
+        color: 'Branco',
+        sex: 'Macho'
+    },
+    {
+        type: 'dog',
+        image: 'zwergspitz.jpg',
+        name: 'Zwergspitz',
+        color: 'Amarelo',
+        sex: 'Fêmea'
+    },
+    {
+        type: 'dog',
+        image: 'husky.jpg',
+        name: 'Husky Siberiano',
+        color: 'Branco e Preto',
+        sex: 'Macho'
+    },
+    {
+        type: 'dog',
+        image: 'golden.jpg',
+        name: 'Golden Retriever',
+        color: 'Amarelo',
+        sex: 'Macho'
+    },
+    {
+        type: 'dog',
+        image: 'poodle.jpg',
+        name: 'Poodle',
+        color: 'Branco',
+        sex: 'Fêmea'
+    },
+    {
+        type: 'dog',
+        image: 'bulldog.jpg',
+        name: 'Bulldog',
+        color: 'Branco e Amarelo',
+        sex: 'Macho'
+    },
+    {
+        type: 'cat',
+        image: 'persa.jpg',
+        name: 'Persa',
+        color: 'Amarelo',
+        sex: 'Macho'
+    },
+    {
+        type: 'cat',
+        image: 'mainecoon.jpg',
+        name: 'Maine Coon',
+        color: 'Preto e Branco',
+        sex: 'Macho'
+    },
+    {
+        type: 'cat',
+        image: 'bengal.jpg',
+        name: 'Bengal',
+        color: 'Branco, Preto e Amarelo',
+        sex: 'Fêmea'
+    },
+    {
+        type: 'cat',
+        image: 'siames.jpg',
+        name: 'Siamês',
+        color: 'Amarelo e Preto',
+        sex: 'Macho'
+    },
+    {
+        type: 'cat',
+        image: 'sphynx.jpg',
+        name: 'Sphynx',
+        color: 'Branco',
+        sex: 'Macho'
+    },
+    {
+        type: 'fish',
+        image: 'neon.jpg',
+        name: 'Tetra Neon',
+        color: 'Vermelho e Azul',
+        sex: 'Macho'
+    },
+    {
+        type: 'fish',
+        image: 'matogrosso.jpg',
+        name: 'Mato Grosso',
+        color: 'Laranja',
+        sex: 'Macho'
+    },
+    {
+        type: 'fish',
+        image: 'limpavidro.jpg',
+        name: 'Limpa Vidro',
+        color: 'Verde e Branco',
+        sex: 'Macho'
+    },
+    {
+        type: 'fish',
+        image: 'tanictis.jpg',
+        name: 'Tanictis',
+        color: 'Vermelho',
+        sex: 'Macho'
+    },
+    {
+        type: 'fish',
+        image: 'acara.jpg',
+        name: 'Acará Bandeira',
+        color: 'Preto',
+        sex: 'Macho'
+    },
+]
+
+export const Pet = {   
+    getAll: (): Pet[] => {
+        return data;
+    },
+    getFromType: (type: PetType): Pet[] => {
+        return data.filter(item => item.type === type);
+        
+        /*  SIMPLIFICADO ACIMA
+        return data.filter(item => {
+            if(item.type === type){
+                return true;
+            }else{
+                return false;
+            }
+        });
+        */
+    },
+    getFromName: (name: string): Pet[] => {
+        return data.filter(item => 
+            item.name.toLowerCase().indexOf(name.toLowerCase()) > -1
+            );
+        
+    }
+};
